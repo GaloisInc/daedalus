d650380f0522fb0d55a27696d640a839d363e701
Iavor Diatchki <iavor.diatchki@gmail.com>
2020-09-18 12:08:42 -0700

    Experiment with a different array representation

--- rts-c/test.cpp
+++ rts-c/test.cpp
@@ -6,6 +6,7 @@
 #include "number.h"
 #include "closure.h"
 #include "iterator.h"
+#include "array.h"
 
 
 class ThisThunk : public Closure {
@@ -22,6 +23,28 @@ int main() {
   mpz_class a = 2;
   mpz_class b = 3;
   std::cout << a + b << std::endl;
+  //
+
+  DDL::ArrayBuilder<int> xs = NULL;
+  xs = new DDL::ArrayCons<int>(1,xs);
+  xs = new DDL::ArrayCons<int>(2,xs);
+  xs = new DDL::ArrayCons<int>(3,xs);
+
+  DDL::Array<int> arr;
+  DDL::buildArray(xs,arr);
+  std::cout << "BUILT" << std::endl;
+  // arr = DDL::Array<int>();
+
+  DDL::Array<int> arr2 = arr;
+  std::cout << "COPIED" << std::endl;
+
+  std::cout << arr2.size() << std::endl;
+
+  DDL::ArrayIterator<int> it(arr);
+  while(!it.done()) {
+    std::cout << it.key() << " = " << it.value() << std::endl;
+    it = it.next();
+  }
 
   return 0;
 }
