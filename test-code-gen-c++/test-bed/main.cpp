ea1cc12daf4f5f7f7effcf60f1b714ac1b989a27
Iavor Diatchki <iavor.diatchki@gmail.com>
2020-09-21 16:01:37 -0700

    Move outside test suite to avoid tests failing

---
+++ test-code-gen-c++/test-bed/main.cpp
@@ -0,0 +1,18 @@
+#include <vector>
+#include <unordered_map>
+#include <memory>
+#include <variant>
+
+#include <parser.h>
+#include <unit.h>
+#include <number.h>
+#include <input.h>
+ 
+// --- Types --- //
+  
+// --- End of Types //
+ 
+// --- Parser --- //
+ 
+void parser(std::vector<DDL::Unit>& output) {
+}
