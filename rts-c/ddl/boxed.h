3d70cca1ba4bcd1ec9c6b0628ceae01120c79fa0
Iavor Diatchki <iavor.diatchki@gmail.com>
2020-10-01 15:34:25 -0700

    A small class for working with reference counted values.

---
+++ rts-c/ddl/boxed.h
@@ -0,0 +1,51 @@
+#ifndef DDL_BOXED
+#define DDL_BOXED
+
+namespace DDL {
+
+
+// A reference counted pointer.   Note that reference counting is done manually
+// using the `copy` and `free` methods and NOT by
+// copying constructors/assignment/destructors.
+// This gives us more precise control over the life times of things.
+template <typename T>
+class Boxed {
+
+  struct BoxedValue {
+    size_t ref_count;
+    T      value;
+    BoxedValue(T &&x) : ref_count(1), value(x) {}
+  } *ptr;
+
+public:
+  Boxed()      : ptr(NULL)                          {}
+  Boxed(T&& x) : ptr (new BoxedValue(std::move(x))) {}
+
+  size_t refCount() { return ptr->ref_count; }
+
+  // Relese this reference to the box.
+  void free() {
+    size_t n = ptr->ref_count;
+    --n;
+    if (n == 0) delete ptr; else ptr->ref_count = n;
+    ptr = NULL;  // just for sanity
+  }
+
+  // Make a new "owned" copy of the referece (i.e., increase ref count).
+  Boxed copy() {
+    ++(ptr->ref_count);
+    return *this;
+  }
+
+  // Get access to the contents of the box.
+  // The resulting reference shouldn't be used after the box is gone.
+  T& operator * () { return ptr->value; }
+};
+
+
+
+}
+
+
+#endif
+
--- rts-c/test.cpp
+++ rts-c/test.cpp
@@ -9,17 +9,32 @@
 #include "ddl/closure.h"
 #include "ddl/array.h"
 #include "ddl/integer.h"
+#include "ddl/boxed.h"
+
+using namespace std;
 
 
 int main() {
-  DDL::Input i;
-  DDL::Parser<int> p(i);
 
-  DDL::Array<DDL::UInt<8>> arr(3,"ABC");
-  std::cout << arr << std::endl;
+  DDL::Boxed<mpz_class> p(mpz_class("123"));
+  DDL::Boxed<mpz_class> q;
+  q = p;
+  cout << p.refCount() << endl;
 
-  DDL::Integer x("123");
-  DDL::Integer y("124");
-  std::cout << (x == y) << std::endl;
+  // DDL::Boxed<mpz_class> p(mpz_class("123"));
+
+  // DDL::Boxed<mpz_class> p(mpz_class("123"));
+  // DDL::Boxed<mpz_class> p = DDL::Boxed(mpz_class("123"));
+  // DDL::Boxed<mpz_class> q = DDL::Boxed(mpz_class("124"));
+  // cout << (p.value() == q.value()) << endl;
+  // DDL::Input i;
+  // DDL::Parser<int> p(i);
+
+  // DDL::Array<DDL::UInt<8>> arr(3,"ABC");
+  // std::cout << arr << std::endl;
+
+  // DDL::Integer x("123");
+  // DDL::Integer y("124");
+  // std::cout << (x == y) << std::endl;
   return 0;
 }
