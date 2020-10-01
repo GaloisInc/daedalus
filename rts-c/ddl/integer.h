8b6a18281d209886682fe5307001b3f536aaf102
Iavor Diatchki <iavor.diatchki@gmail.com>
2020-10-01 16:58:52 -0700

    Redo `integer` to use the new boxed type.

--- rts-c/ddl/integer.h
+++ rts-c/ddl/integer.h
@@ -1,73 +1,120 @@
 #ifndef DDL_INTEGER
 #define DDL_INTEGER
 
-#include <memory>
 #include <gmpxx.h>
+#include <ddl/boxed.h>
 
 namespace DDL {
 
-// This ends up with two pointers:
-// we have a pointer to an mpt_z, which contains a pointer to the libs.
-class Integer {
-  std::shared_ptr<mpz_class> data;
+class Integer : public Boxed<mpz_class> {
+
 public:
-  Integer(const char* str) : data(std::make_shared<mpz_class>(str)) {}
-  Integer(std::shared_ptr<mpz_class> x) : data(x) {}
-
-  template <int op>
-
-  Integer operator + (Integer x) {
-    return std::make_shared<mpz_class>(*data + *x.data);
-  }
-
-  Integer operator - (Integer x) {
-    return std::make_shared<mpz_class>(*data - *x.data);
-  }
-
-  Integer operator * (Integer x) {
-    return std::make_shared<mpz_class>(*data * *x.data);
-  }
-
-  // XXX: check for division by 0?
-  Integer operator / (Integer x) {
-    return std::make_shared<mpz_class>(*data / *x.data);
-  }
-
-  Integer operator - () {
-    return std::make_shared<mpz_class>(- *data);
-  }
-
-  bool operator == (Integer x) { return *data == *x.data; }
-  bool operator != (Integer x) { return *data != *x.data; }
-  bool operator <  (Integer x) { return *data <  *x.data; }
-  bool operator <= (Integer x) { return *data <= *x.data; }
-
-  // Assumes `x.data` is not NULL
-  friend
-  std::ostream& operator<<(std::ostream& os, Integer x) {
-    os << *x.data;
-    return os;
-  }
-
-  friend std::hash<Integer>;
+  Integer(const char* str)    : Boxed<mpz_class>(mpz_class(str)) {}
+  Integer()                   : Boxed<mpz_class>()               {}
+  Integer(Boxed<mpz_class> x) : Boxed<mpz_class>(x)              {}
+  Integer(mpz_class &&x)      : Boxed<mpz_class>(std::move(x))   {}
 };
 
+// borrow
+static inline
+bool operator == (Integer x, Integer y) { return x.getValue() == y.getValue(); }
+
+// borrow
+static inline
+bool operator != (Integer x, Integer y) { return x.getValue() != y.getValue(); }
+
+// borrow
+static inline
+bool operator <  (Integer x, Integer y) { return x.getValue() <  y.getValue(); }
+
+// borrow
+static inline
+bool operator <= (Integer x, Integer y) { return x.getValue() <= y.getValue(); }
+
+// borrow
+static inline
+std::ostream& operator<<(std::ostream& os, Integer x) {
+  os << x.getValue();
+  return os;
 }
 
-namespace std {
-  template<>
-  struct hash<DDL::Integer> {
 
-    size_t operator()(DDL::Integer x) const noexcept {
-      size_t result = 17;
-      mpz_srcptr info = x.data->get_mpz_t();
-      size_t n = abs(info->_mp_size);
-      for (size_t i = 0; i < n; ++i)
-        result = 23 * result + (size_t)(info->_mp_d[i]);
-      return result;
-    }
-  };
+// owned
+static inline
+Integer operator + (Integer x, Integer y) {
+  mpz_class &xv = x.getValue();
+  mpz_class &yv = y.getValue();
+  if (x.refCount() == 1) { xv += yv; y.free(); return x; }
+  if (y.refCount() == 1) { yv += xv; x.free(); return y; }
+  Integer z(xv + yv);
+  x.free(); y.free();
+  return z;
 }
+
+// owned
+static inline
+Integer operator - (Integer x, Integer y) {
+  mpz_class &xv = x.getValue();
+  mpz_class &yv = y.getValue();
+  if (x.refCount() == 1) { xv -= yv; y.free(); return x; }
+  if (y.refCount() == 1) { yv = xv - yv; x.free(); return y; }
+  Integer z(xv - yv);
+  x.free(); y.free();
+  return z;
+}
+
+// owned
+static inline
+Integer operator * (Integer x, Integer y) {
+  mpz_class &xv = x.getValue();
+  mpz_class &yv = y.getValue();
+  if (x.refCount() == 1) { xv *= yv; y.free(); return x; }
+  if (y.refCount() == 1) { yv *= xv; x.free(); return y; }
+  Integer z(xv * yv);
+  x.free(); y.free();
+  return z;
+}
+
+// XXX: Check for division by 0?
+// owned
+static inline
+Integer operator / (Integer x, Integer y) {
+  mpz_class &xv = x.getValue();
+  mpz_class &yv = y.getValue();
+  if (x.refCount() == 1) { xv /= yv;     y.free(); return x; }
+  if (y.refCount() == 1) { yv = xv / yv; x.free(); return y; }
+  Integer z(xv / yv);
+  x.free(); y.free();
+  return z;
+}
+
+
+// XXX: Check for division by 0?
+// owned
+static inline
+Integer operator % (Integer x, Integer y) {
+  mpz_class &xv = x.getValue();
+  mpz_class &yv = y.getValue();
+  if (x.refCount() == 1) { xv %= yv;     y.free(); return x; }
+  if (y.refCount() == 1) { yv = xv % yv; x.free(); return y; }
+  Integer z(xv % yv);
+  x.free(); y.free();
+  return z;
+}
+
+static inline
+// owned
+Integer operator - (Integer x) {
+  mpz_class &v = x.getValue();
+  if (x.refCount() == 1) { v = -v; return x; }
+  Integer y(-v);
+  x.free();
+  return y;
+}
+
+
+}
+
 
 
 #endif
--- rts-c/test.cpp
+++ rts-c/test.cpp
@@ -16,25 +16,8 @@ using namespace std;
 
 int main() {
 
-  DDL::Boxed<mpz_class> p(mpz_class("123"));
-  DDL::Boxed<mpz_class> q;
-  q = p;
-  cout << p.refCount() << endl;
-
-  // DDL::Boxed<mpz_class> p(mpz_class("123"));
-
-  // DDL::Boxed<mpz_class> p(mpz_class("123"));
-  // DDL::Boxed<mpz_class> p = DDL::Boxed(mpz_class("123"));
-  // DDL::Boxed<mpz_class> q = DDL::Boxed(mpz_class("124"));
-  // cout << (p.value() == q.value()) << endl;
-  // DDL::Input i;
-  // DDL::Parser<int> p(i);
-
-  // DDL::Array<DDL::UInt<8>> arr(3,"ABC");
-  // std::cout << arr << std::endl;
-
-  // DDL::Integer x("123");
-  // DDL::Integer y("124");
-  // std::cout << (x == y) << std::endl;
+  DDL::Integer x("123");
+  DDL::Integer y(mpz_class(123));
+  cout << (x == y) << endl;
   return 0;
 }
