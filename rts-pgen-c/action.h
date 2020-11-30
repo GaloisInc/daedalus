64264d9f7e837aee000009cae00f8c6346da498f
Hari Menon <hari@galois.com>
2020-11-30 03:46:39 -0500

    Refactor actions to a more appropriate header

--- rts-pgen-c/action.h
+++ rts-pgen-c/action.h
@@ -3,8 +3,57 @@
 
 #include <stdio.h>
 
-#include "aut.h"
 #include "cfg.h"
+
+typedef enum _ActionType {
+    ACT_EpsA,
+    ACT_ReadChar,
+    ACT_Match,
+    ACT_Push,
+    ACT_Pop,
+    ACT_ReturnBind,
+    ACT_EnvFresh,
+    ACT_EnvStore,
+    ACT_ActivateFrame,
+    ACT_DeactivateReady,
+    ACT_END
+} ActionType ;
+
+typedef enum _ExprType {
+    E_INT,
+    E_STR,
+    E_VAR
+} ExprType ;
+
+typedef struct _Expr {
+    ExprType tag;
+    union {
+        int vInt;
+        char* vBytes;
+        char* name;
+    };
+} Expr ;
+
+typedef struct _NameList {
+
+} NameList ;
+
+typedef struct KeyValuePair {
+    char * key;
+    Value * value;
+} KeyValue;
+
+typedef struct _Action {
+    ActionType tag;
+    union {
+        char chr;
+        int state;
+        KeyValue *kv;
+        Expr* expr;
+        char* name;
+        NameList* namelist;
+    };
+} Action;
 
 /** Execute an action */
 Cfg * execAction(Action * act, Cfg* cfg, int arrivState);
