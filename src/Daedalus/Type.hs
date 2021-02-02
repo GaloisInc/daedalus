680cf4e58676fe99b50d491b4e032010bf8d045f
Simon Winwood <simonjwinwood@gmail.com>
2021-02-01 23:02:23 -0800

    Translate ' is ' into TCCase (for unions)

--- src/Daedalus/Type.hs
+++ src/Daedalus/Type.hs
@@ -774,12 +774,17 @@ inferExpr expr =
                     addConstraint f (HasStruct t lab a)
                     pure (exprAt expr (TCSelStruct e1 lab a), a)
 
+               -- turn 'e is l' into 'case e is { l x => ^ x }'
                SelUnion f ->
                  grammarOnly expr $
                  liftValApp expr [e] \ ~[(e1,t)] ->
                  do let lab = thingValue f
                     addConstraint f (HasUnion t lab a)
-                    pure (exprAt expr (TCSelUnion YesSem e1 lab a), tGrammar a)
+                    resVar <- newName expr a 
+                    let pat = TCConPat t lab (TCVarPat resVar)
+                        alt = TCAlt [pat] (exprAt expr $ TCPure
+                                          $ exprAt expr $ TCVar resVar)
+                    pure (exprAt expr (TCCase e1 (alt :| []) Nothing), tGrammar a)
 
                SelTrue ->
                  grammarOnly expr $
