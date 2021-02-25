eb298919a370dc255c6c4d8b1c5eb26f7cd9fc23
Iavor Diatchki <iavor.diatchki@gmail.com>
2021-02-25 09:01:19 -0800

    Wibbles

--- daedalus-utils/src/Daedalus/GUID.hs
+++ daedalus-utils/src/Daedalus/GUID.hs
@@ -16,7 +16,7 @@ newtype GUID = GUID { getGUID :: Int }
   deriving (Ord, Eq, Show)
 
 instance PP GUID where
-  pp (GUID n) = text (show n)
+  pp (GUID n) = int n
 
 -- Used before we have resolved scope
 invalidGUID :: GUID
@@ -29,10 +29,12 @@ succGUID :: GUID -> GUID
 succGUID guid = GUID (getGUID guid + 1)
 
 
-mkGUIDState :: StateM m s => (s -> GUID) -> (GUID -> s -> s) -> (GUID -> (a, GUID)) -> m a
+mkGUIDState ::
+  StateM m s => (s -> GUID) -> (GUID -> s -> s) -> (GUID -> (a, GUID)) -> m a
 mkGUIDState proj inj f = sets (mkGUIDState' proj inj f)
 
-mkGUIDState' :: (s -> GUID) -> (GUID -> s -> s) -> (GUID -> (a, GUID)) -> (s -> (a, s))
+mkGUIDState' ::
+  (s -> GUID) -> (GUID -> s -> s) -> (GUID -> (a, GUID)) -> (s -> (a, s))
 mkGUIDState' proj inj = \f s -> let (r, guid') = f (proj s)
                                 in (r, inj guid' s)
 
@@ -51,15 +53,11 @@ instance (HasGUID m) => HasGUID (ReaderT i m) where
 instance (HasGUID m,Monoid i) => HasGUID (WriterT i m) where
   guidState = lift . guidState
 instance (HasGUID m) => HasGUID (StateT s m) where
-  guidState = lift . guidState  
+  guidState = lift . guidState
 instance (HasGUID m) => HasGUID (ExceptionT i m) where
   guidState = lift . guidState
 instance (HasGUID m) => HasGUID (ChoiceT m) where
   guidState = lift . guidState
 instance (HasGUID m) => HasGUID (ContT i m) where
   guidState = lift . guidState
-
-
-  
-  
 
