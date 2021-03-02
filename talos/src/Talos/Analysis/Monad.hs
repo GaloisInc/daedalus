3f4db939da6faf20c3598c554a9e5b156673d0df
Simon Winwood <simonjwinwood@gmail.com>
2021-03-01 23:40:03 -0800

    talos: Updated Analysis monad for Core

--- talos/src/Talos/Analysis/Monad.hs
+++ talos/src/Talos/Analysis/Monad.hs
@@ -11,9 +11,8 @@ import qualified Data.Set as Set
 
 import Control.Monad.State
 
-import Daedalus.Type.AST
+import Daedalus.Core
 import Daedalus.PP
-import Daedalus.Pass
 import Daedalus.GUID
 
 import Talos.Analysis.Slice
@@ -21,21 +20,21 @@ import Talos.Analysis.Domain
 
 -- This is the current map from variables to path sets that begin at
 -- that variable.  We assume that variables are (globally) unique.
-type PathRootMap =  Map (TCName Value) (Slice TCSynthAnnot)
+type PathRootMap =  Map Name Slice
 
 -- This is the summarisation for a given class for a given function
 data Summary =
-  Summary { exportedDomain   :: Domain TCSynthAnnot
+  Summary { exportedDomain   :: Domain
           -- ^ If this is a function result summary, this will contain
           -- an entry for ResultVar, including any params entangled
           -- with the result.
           , pathRootMap :: PathRootMap
           -- Essentially a copy of the params in the decl.
-          , params      :: [TCName Value]
+          , params      :: [Name]
           , summaryClass :: SummaryClass 
           }
 
-type Summaries    = Map Name (Map SummaryClass Summary)
+type Summaries    = Map FName (Map SummaryClass Summary)
 -- type Summaries    = Map Name Summary
 
 --------------------------------------------------------------------------------
@@ -46,18 +45,16 @@ type Summaries    = Map Name (Map SummaryClass Summary)
 -- change, so the type of summary requested is included.  Note that a
 -- name may appear multiole times if multiple (different) summaries
 -- are requested.
-type RevDeps = Map (Name, SummaryClass) (Set (Name, SummaryClass))
+type RevDeps = Map (FName, SummaryClass) (Set (FName, SummaryClass))
              
-type Worklist = Set (Name, SummaryClass)
+type Worklist = Set (FName, SummaryClass)
 
-data IterState = {- forall a. -} IterState
+data IterState =  IterState
   { worklist  :: Worklist
   , revDeps   :: RevDeps
   , summaries :: Summaries
-  , allDecls  :: Map Name (TCDecl TCSynthAnnot) -- read-only
-  -- For missingPatternsTC
-  , declaredTys :: Map TCTyName TCTyDecl
-  , currentDecl  :: Name
+  , allDecls  :: Map FName (Fun Grammar) -- read-only
+  , currentDecl  :: FName
   , currentClass :: SummaryClass
   , nextGUID    :: GUID
   }
@@ -65,13 +62,12 @@ data IterState = {- forall a. -} IterState
 -- We want assertions for all decls as the base case for calls is to
 -- use 'Assertion although we may not always require them (if every
 -- call to a function uses the result in a relevant way)
-initState :: Map TCTyName TCTyDecl -> [TCDecl TCSynthAnnot] -> GUID -> IterState
-initState declTys decls nguid = IterState
+initState :: [Fun Grammar] -> GUID -> IterState
+initState decls nguid = IterState
   { worklist    = Set.fromList grammarDecls
   , revDeps     = Map.empty
   , summaries   = Map.empty
-  , allDecls    = Map.fromList (map (\tc -> (tcDeclName tc, tc)) decls)
-  , declaredTys = declTys
+  , allDecls    = Map.fromList (map (\tc -> (fName tc, tc)) decls)
   , currentDecl  = error "No current decl"
   , currentClass = error "No current class"
   , nextGUID    = nguid
@@ -79,9 +75,8 @@ initState declTys decls nguid = IterState
   where
     grammarDecls =
       [ (name, Assertions)
-      | TCDecl { tcDeclName = name, tcDeclDef = Defined _, tcDeclCtxt = AGrammar } <- decls
+      | Fun { fName = name, fDef = Def _ } <- decls
       ]
-
 
 -- We pretend this is a DDL pass to get e.g. fresh name support  
 newtype IterM a = IterM { getIterM :: State IterState a }
@@ -96,49 +91,38 @@ runIterM m s0 =  execState (getIterM m) s0
 instance HasGUID IterM where
   guidState f = IterM $ state (mkGUIDState' nextGUID (\v s -> s { nextGUID = v }) f)
 
-currentDeclName :: IterM Name
+currentDeclName :: IterM FName
 currentDeclName = IterM $ gets currentDecl
 
 currentSummaryClass :: IterM SummaryClass
 currentSummaryClass = IterM $ gets currentClass
 
-declaredTypes :: IterM (Map TCTyName TCTyDecl)
-declaredTypes = IterM $ gets declaredTys
-
-addRevDep :: Name -> SummaryClass -> IterM ()
+addRevDep :: FName -> SummaryClass -> IterM ()
 addRevDep nm cl = do
   here    <- currentDeclName
   here_cl <- currentSummaryClass
   IterM $ modify (\s -> s { revDeps = Map.insertWith Set.union (nm, cl) (Set.singleton (here, here_cl)) (revDeps s) })
 
-getRevDeps :: Name -> SummaryClass -> IterM (Set (Name, SummaryClass))
+getRevDeps :: FName -> SummaryClass -> IterM (Set (FName, SummaryClass))
 getRevDeps nm cl = do
   IterM $ Map.findWithDefault Set.empty (nm, cl) <$> gets revDeps
 
-mkResultVar :: Name -> IterM (TCName Value)
-mkResultVar fname = do
-  decl <- getDecl fname
-  case decl of
-    TCDecl { tcDeclDef = Defined d } ->
-      freshLocalTCName "$result" (typeOf d) AValue
-    _ -> error "External function"
-
-getDecl :: Name -> IterM (TCDecl TCSynthAnnot)
+getDecl :: FName -> IterM (Fun Grammar)
 getDecl n = IterM $ gets (flip (Map.!) n . allDecls)
 
-lookupSummary :: Name -> SummaryClass -> IterM (Maybe Summary)
+lookupSummary :: FName -> SummaryClass -> IterM (Maybe Summary)
 lookupSummary nm cls = do
   m_summary <- IterM $ gets (Map.lookup nm . summaries)
   pure $ Map.lookup cls =<< m_summary 
     
-requestSummarisation :: Name -> SummaryClass -> IterM ()
+requestSummarisation :: FName -> SummaryClass -> IterM ()
 requestSummarisation nm p =
   IterM $ modify (\s -> s { worklist = Set.insert (nm, p) (worklist s) })
 
 -- IterM interface
 
 -- Gets the precondition for a given decl.  This may update the worklist and revdeps
-requestSummary :: Name -> SummaryClass -> IterM Summary
+requestSummary :: FName -> SummaryClass -> IterM Summary
 requestSummary nm cl = do
   addRevDep nm cl
   m_summary <- lookupSummary nm cl
@@ -154,7 +138,7 @@ requestSummary nm cl = do
 -- changed, so they need to be resummarised.
 
 -- Note: Everything in revdeps will have a summary
-propagate :: Name -> SummaryClass -> IterM ()
+propagate :: FName -> SummaryClass -> IterM ()
 propagate nm cl = do
   rdeps  <- getRevDeps nm cl
   IterM $ modify (\s -> s { worklist = Set.union rdeps (worklist s) })
