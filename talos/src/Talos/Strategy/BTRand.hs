1965e1024340ee80857f28ef42221936fb466e76
Simon Winwood <simonjwinwood@gmail.com>
2021-03-25 16:29:32 -0700

    talos: Moved DFST into its own module.

--- talos/src/Talos/Strategy/BTRand.hs
+++ talos/src/Talos/Strategy/BTRand.hs
@@ -23,6 +23,7 @@ import Talos.Analysis.EntangledVars
 import Talos.Analysis.Slice
 import Talos.SymExec.Path
 import Talos.Strategy.Monad
+import Talos.Strategy.DFST
 
 -- ----------------------------------------------------------------------------------------
 -- Backtracking random strats
@@ -147,76 +148,3 @@ enumerate :: Traversable t => t a -> t (Int, a)
 enumerate t = evalState (traverse go t) 0
   where
     go a = state (\i -> ((i, a), i + 1))
-
-
--- =============================================================================
--- DFS monad transformer
---
--- This is similar to the list monad, but it wraps another monad and
--- hence has to be a bit careful about what to do when --- if we use
--- ListT, we get effects from all the alternatives, which could be
--- expensive.  This is similar to ContT, but we also keep around a
--- failure continuation.
---
-
--- The dfsCont takes the return value, and also an updated failure
--- continuation, as we may want to backtrack into a completed
--- computation.
-data DFSTContext r m a =
-  DFSTContext { dfsCont :: a -> m r -> m r
-              , dfsFail :: m r
-              }
-
-newtype DFST r m a = DFST { getDFST :: DFSTContext r m a -> m r }
-
-runDFST :: DFST r m a -> (a -> m r) -> m r -> m r
-runDFST m cont fl = getDFST m (DFSTContext (\v _ -> cont v) fl)
-
-instance Functor (DFST r m) where
-  fmap f (DFST m) = DFST $ \ctxt -> m (ctxt { dfsCont = dfsCont ctxt . f })
-
-instance Applicative (DFST r m) where
-  pure v              = DFST $ \ctxt -> dfsCont ctxt v (dfsFail ctxt)
-  (<*>)               = ap
-  -- DFST fm <*> DFST vm = DFST $ \ctxt ->
-  --   let vCont f = \v -> dfsCont ctxt (f v)
-  --       fCont   = \f -> vm (ctxt { dfsCont = vCont f })
-  --   in fm (ctxt { dfsCont = fCont })
-
-instance Monad (DFST r m) where
-  DFST m >>= f = DFST $ \ctxt ->
-    let cont v fl = getDFST (f v) ( ctxt { dfsFail = fl })
-    in m (ctxt { dfsCont = cont })
-
--- | We want
---
--- (a `mplus` b) >>= f == (a >>= f) `mplus` (b >>= f)
---
--- i.e., we give f the result of a, but if that fails, we run f on b's
--- result.
-
-instance Alternative (DFST r m) where
-  (DFST m1) <|> (DFST m2) = DFST $ \ctxt ->
-    let ctxt1 = ctxt { dfsFail = m2 ctxt } in m1 ctxt1
-  empty = DFST dfsFail 
-
-instance MonadPlus (DFST r m) where -- default body (Alternative)
-                     
-instance MonadTrans (DFST r) where
-  lift m = DFST $ \ctxt -> m >>= \v -> dfsCont ctxt v (dfsFail ctxt)
-  
-instance LiftStrategyM m => LiftStrategyM (DFST r m) where
-  liftStrategy m = lift (liftStrategy m)
-    
-    
-
-
-
-
-
-        
-        
-        
-
-          
-
