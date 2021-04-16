292405cc210fdea151ea4a38282629abb7c57706
Benoit Razet <benoit.razet@galois.com>
2021-04-16 13:13:27 -0400

    Remove panic and complete Value comparison
    It removes the little bit of type-checking. It is useful to keep the
    comparison total for LL(*) which uses hashconsing with maps on values.

--- daedalus-value/src/Daedalus/Value/Compare.hs
+++ daedalus-value/src/Daedalus/Value/Compare.hs
@@ -3,20 +3,19 @@ module Daedalus.Value.Compare (vEq, vNeq, vLt, vGt, vLeq, vGeq) where
 import Daedalus.Value.Type
 
 vEq :: Value -> Value -> Value
-vEq x y = VBool (vCompare "==" x y == EQ)
+vEq x y = VBool (vCompare x y == EQ)
 
 vNeq :: Value -> Value -> Value
-vNeq x y = VBool (vCompare "!=" x y /= EQ)
+vNeq x y = VBool (vCompare x y /= EQ)
 
 vLt :: Value -> Value -> Value
-vLt x y = VBool (vCompare "<" x y == LT)
+vLt x y = VBool (vCompare x y == LT)
 
 vGt :: Value -> Value -> Value
-vGt x y = VBool (vCompare ">" x y == GT)
+vGt x y = VBool (vCompare x y == GT)
 
 vLeq :: Value -> Value -> Value
-vLeq x y = VBool (vCompare "<=" x y /= GT)
+vLeq x y = VBool (vCompare x y /= GT)
 
 vGeq :: Value -> Value -> Value
-vGeq x y = VBool (vCompare ">=" x y /= LT)
-
+vGeq x y = VBool (vCompare x y /= LT)
