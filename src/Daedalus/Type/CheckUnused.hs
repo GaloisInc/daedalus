3d2b75bfa0a443bb3be5cab1d9e40c146c999ce5
Valentin Robert <val@galois.com>
2021-07-09 10:06:02 -0700

    daedalus: avoid accidental Haddock comments
    Commented-out lines that start with a `|` look like the beginning of Haddock
    documentation comments, and make it so that Haddock fails to build
    documentation for this project.  Adding a whitespace is enough to keep it
    happy.

--- src/Daedalus/Type/CheckUnused.hs
+++ src/Daedalus/Type/CheckUnused.hs
@@ -22,8 +22,8 @@ checkTCDecl TCDecl { tcDeclCtxt, tcDeclDef } =
 
 checkTC :: Bool -> TC SourceRange Grammar -> [SourceRange]
 checkTC triv tc =
-  -- | trace (show (triv, pp tc)) False = undefined
-  -- | otherwise =
+  --  | trace (show (triv, pp tc)) False = undefined
+  --  | otherwise =
   let warnIf b = if b then [ texprAnnot tc ] else []
       okLeaf   = []
   in
