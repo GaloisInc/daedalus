83897b1bd7dbf4f98aeb96acff64045a7c96c021
Iavor Diatchki <iavor.diatchki@gmail.com>
2021-08-20 14:45:46 +0300

    Signed tests

--- rts-c/test_size.cpp
+++ rts-c/test_size.cpp
@@ -8,8 +8,17 @@ void test_size() {
   uint32_t x32 = std::numeric_limits<uint32_t>::max();
   uint64_t x64 = std::numeric_limits<uint64_t>::max();
 
-  std::cout << DDL::Size(x8) << " "
-            << DDL::Size(x16) << " "
-            << DDL::Size(x32) << " "
-            << DDL::Size(x64) << "\n";
+  int8_t  s8  = std::numeric_limits<int8_t>::max();
+  int16_t s16 = std::numeric_limits<int16_t>::max();
+  int32_t s32 = std::numeric_limits<int32_t>::max();
+  int64_t s64 = std::numeric_limits<int64_t>::max();
+
+  std::cout << DDL::Size{ x8} << " "
+            << DDL::Size{x16} << " "
+            << DDL::Size{x32} << " "
+            << DDL::Size{x64} << "\n"
+            << DDL::Size{s8} << " "
+            << DDL::Size{s16} << " "
+            << DDL::Size{s32} << " "
+            << DDL::Size{s64} << "\n";
 }
