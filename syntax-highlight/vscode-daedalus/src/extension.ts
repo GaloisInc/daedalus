fb8f2fc5a79a4ae12b1bb3c087d6095884f1b158
Simon Winwood <simonjwinwood@gmail.com>
2021-11-03 09:23:16 -0700

    lsp: bugfixes for highlighting

--- syntax-highlight/vscode-daedalus/src/extension.ts
+++ syntax-highlight/vscode-daedalus/src/extension.ts
@@ -105,7 +105,7 @@ export function activate(context: vscode.ExtensionContext) {
 		clientOptions
 	)
 
-	client.registerFeature(new SemanticTokensFeature())
+	// client.registerFeature(new SemanticTokensFeature())
 
 	const runWatchProvider = new DaedalusWatchContentProvider()
 
