6f1a4cfdb0aa5bd9ec3762f80ae13707db8b96c2
Mark Tullsen <tullsen@galois.com>
2022-03-05 15:54:29 -0800

    testing framework tweaks, new scripts

---
+++ pdf-driver/scripts/pdf-driver-test.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+MY_PATH=$(dirname "$0")
+TEST_DIR=$MY_PATH/../test
+
+if [ $# == 1 -a \( "$1" = "quick" -o "$1" = "long" \) ] ; then
+  TARGET=$1
+else
+  echo "Usage: pdf-driver-test.sh {quick,long}"
+  exit 1
+fi
+
+cd $TEST_DIR
+
+make $TARGET-status
+
+if [ $? -eq 0 ]
+then
+    echo
+    echo pdf-hs-driver $TARGET tests GOOD: all variances accounted for.
+    exit 0
+else
+    echo
+    echo pdf-hs-driver $TARGET tests ERROR: unaccounted for variances.
+    exit 1
+fi
