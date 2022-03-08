569e4c8f59bd5f9ce0f99004b06128fe87c5ff7c
Eric Mertens <emertens@galois.com>
2022-03-08 15:31:17 -0800

    Lazy parsing of object stream elements

--- pdf-cos/c++/src/state.cpp
+++ pdf-cos/c++/src/state.cpp
@@ -1,5 +1,8 @@
 #include "state.hpp"
 
+ReferenceEntry::ReferenceEntry(oref value, generation_type gen)
+: value(value), gen(gen) {}
+
 TopThunk::TopThunk(uint64_t offset) : offset(offset) {}
 
 bool
@@ -27,11 +30,11 @@ TopThunk::getDecl(DDL::Input input, User::TopDecl *result)
     return true;
 }
 
-StreamThunk::StreamThunk(uint64_t refid, uint64_t container, uint64_t index)
-: refid(refid), container(container), index(index) {}
+StreamThunk::StreamThunk(uint64_t container, uint64_t index)
+: container(container), index(index) {}
 
 bool
-StreamThunk::getDecl(DDL::Input input, User::TopDecl *result)
+StreamThunk::getDecl(DDL::Input input, uint64_t refid, User::TopDecl *result)
 {
     DDL::Maybe<User::TopDecl> streamResult;
     if (!references.resolve_reference(input, container, 0, &streamResult)) {
@@ -50,29 +53,34 @@ StreamThunk::getDecl(DDL::Input input, User::TopDecl *result)
     auto stream = borrowed(streamResult.borrowValue().borrow_obj().borrow_objstream());
     streamResult.free();
 
-    if (index >= stream->size().value) {
+    if (index >= stream->borrow_index().size().value) {
+        return false;
+    }
+
+    auto byteOffset = stream->borrow_index().borrowElement(index).borrow_off().asSize();
+    auto objInput = stream->get_bytes().iDrop(byteOffset);
+    
+    std::vector<User::Value> results;
+    DDL::ParseError error;
+    parseValue(objInput, error, results);
+
+    if (1 != results.size()) {
+        for (auto && x : results) { x.free(); }
         return false;
     }
 
     User::TopDeclDef topDef;
-    topDef.init_value(stream.borrow()[index]);
+    topDef.init_value(results[0]);
     result->init(refid, 0, topDef);
 
     return true;
 }
 
-const char *
-BlackholeException::what() const throw ()
-{
-    return "Entered a blackhole resolving references";
-}
-
 // Owns topDecl
 void
-ReferenceTable::register_topdecl(uint64_t refid, uint16_t gen, User::TopDecl topDecl)
+ReferenceTable::register_topdecl(uint64_t refid, generation_type gen, User::TopDecl topDecl)
 {
-    auto key = std::make_pair(refid, gen);
-    table.insert_or_assign(key, owned(topDecl));
+    table.insert_or_assign(refid, ReferenceEntry{owned(topDecl), gen});
 }
 
 void
@@ -82,40 +90,38 @@ ReferenceTable::register_compressed_reference(
     uint64_t index
 )
 {
-    // XXX: Handle overwrite: need to .free(), etc.
-    table.insert(
-        std::make_pair(std::make_pair(refid, 0), StreamThunk(refid, container, index))
-    );
+    table.insert_or_assign(refid, ReferenceEntry{StreamThunk{container, index}, 0});
 }
 
 void
 ReferenceTable::register_uncompressed_reference(
     uint64_t refid,
-    uint16_t gen,
+    generation_type gen,
     uint64_t offset
 )
 {
-    // XXX: Handle overwrite: need to .free(), etc.
-    table.insert(
-        std::make_pair(std::make_pair(refid, gen), TopThunk(offset))
-    );
+    table.insert_or_assign(refid, ReferenceEntry{TopThunk(offset), 0});
+}
+
+void
+ReferenceTable::unregister(uint64_t refid)
+{
+    table.erase(refid);
 }
 
 bool
 ReferenceTable::resolve_reference(
-    DDL::Input input, uint64_t refid, uint16_t gen, DDL::Maybe<User::TopDecl> *result
+    DDL::Input input, uint64_t refid, generation_type gen, DDL::Maybe<User::TopDecl> *result
 ) {
-    auto key = std::make_pair(refid, gen);
+    auto cursor = table.find(refid);
 
-    auto cursor = table.find(key);
-
-    if (cursor == std::end(table)) {
+    if (cursor == std::end(table) || cursor->second.gen != gen) {
         *result = DDL::Maybe<User::TopDecl>();
         return true;
     }
 
     // Copy the entry by value so that we can replace it with a blackhole if needed
-    auto entry = cursor->second;
+    auto entry = cursor->second.value;
 
     std::cerr << "Resolving reference " << refid << " with thunk type " << entry.index() << std::endl;
 
@@ -129,22 +135,22 @@ ReferenceTable::resolve_reference(
     }
 
     if (auto *thunk = std::get_if<TopThunk>(&entry)) {
-        cursor->second = Blackhole();
+        cursor->second.value = Blackhole();
         User::TopDecl decl;
         bool success = thunk->getDecl(input, &decl);
         if (success) {
-            cursor->second = borrowed(decl);
+            cursor->second.value = borrowed(decl);
             *result = decl;
         }
         return success;
     }
 
     if (auto *thunk = std::get_if<StreamThunk>(&entry)) {
-        cursor->second = Blackhole();
+        cursor->second.value = Blackhole();
         User::TopDecl decl;
-        bool success = thunk->getDecl(input, &decl);
+        bool success = thunk->getDecl(input, refid, &decl);
         if (success) {
-            cursor->second = borrowed(decl);
+            cursor->second.value = borrowed(decl);
             *result = decl;
         }
         return success;
@@ -187,14 +193,13 @@ void process_newXRef(std::unordered_set<size_t> *visited, DDL::Input input, User
                     uint64_t container = compressed.borrow_container_obj().asSize().value;
                     uint64_t obj_index = compressed.borrow_obj_index().asSize().value;
 
-                    std::cerr << "Adding compressed xref " << refid << " at " << obj_index << std::endl;
-
                     references.register_compressed_reference(refid, container, obj_index);
                     break;
                 }
-                case DDL::Tag::XRefObjEntry::free:
-                    // XXX: do something?
+                case DDL::Tag::XRefObjEntry::free: {
+                    references.unregister(entry->borrow_free().borrow_obj().asSize().value);
                     break;
+                }
 
                 case DDL::Tag::XRefObjEntry::inUse: {
                     auto inUse = entry->borrow_inUse();
@@ -203,7 +208,7 @@ void process_newXRef(std::unordered_set<size_t> *visited, DDL::Input input, User
                     uint64_t offset = inUse.borrow_offset().asSize().value;
 
                     // maximum generation number is 65,535
-                    uint16_t gen = inUse.borrow_gen().getValue().get_ui();
+                    generation_type gen = inUse.borrow_gen().getValue().get_ui();
 
                     std::cerr << "Adding uncompressed xref " << refid << " " << gen << " at " << offset << std::endl;
 
@@ -249,23 +254,21 @@ void process_oldXRef(std::unordered_set<size_t> *visited, DDL::Input input, User
 
             switch (entry->getTag()) {
                 case DDL::Tag::CrossRefEntry::inUse: {
+                    auto isUse = entry->borrow_inUse();
 
-                auto isUse = entry->borrow_inUse();
+                    // 10 digit natural fits in 34 bits
+                    uint64_t offset = isUse.borrow_offset().asSize().value;
 
-                // 10 digit natural fits in 34 bits
-                uint64_t offset = isUse.borrow_offset().asSize().value;
+                    // maximum generation number is 65,535
+                    generation_type gen = isUse.borrow_gen().getValue().get_ui();
 
-                // maximum generation number is 65,535
-                uint16_t gen = isUse.borrow_gen().getValue().get_ui();
-
-                std::cerr << "Adding xref " << refid << " " << gen << " at " << offset << std::endl;
-
-                references.register_uncompressed_reference(refid, gen, offset);
-                break;
+                    references.register_uncompressed_reference(refid, gen, offset);
+                    break;
                 }
+
                 case DDL::Tag::CrossRefEntry::free: {
-                // XXX: implement
-                break;
+                    references.unregister(entry->borrow_free().borrow_obj().asSize().value);
+                    break;
                 }
             }
 
