448de702c91a0dd0961bb745e1c82fa336d54869
Eric Mertens <emertens@galois.com>
2022-03-08 10:43:34 -0800

    big PDF is parsing

--- pdf-cos/c++/src/Owned.hpp
+++ pdf-cos/c++/src/Owned.hpp
@@ -1,30 +1,107 @@
 #pragma once
 
+
+template <class T>
+class Owned;
+
+template <class T>
+Owned<T> owned(T x);
+
+template <class T>
+Owned<T> borrowed(T x);
+
 template<class T>
 class Owned {
-    T x;
+    T obj;
+
+    // Owns x
+    explicit Owned(T x);
+    Owned() = delete;
 
 public:
-    // Consumes an owned value
-    Owned(T x);
+
+    Owned(const Owned<T> &x);
+    //Owned(Owned &&x);
 
     // Frees owned value
     ~Owned();
 
+    Owned &operator=(const Owned &x);
+
     // Accesses underlying owned value
     T* operator->();
+    T const* operator->() const;
 
     T borrow() const;
+    T get() const;
+
+    friend Owned<T> owned<T>(T);
+    friend Owned<T> borrowed<T>(T);
 };
 
 template <class T>
-Owned<T>::Owned(T x) : x(x) {}
+T Owned<T>::get() const
+{
+    T r = obj;
+    r.copy();
+    return r;
+}
+
+// Takes ownership of its argument
+template <class T>
+Owned<T> owned(T x)
+{
+    return Owned{x};
+}
+
+// Borrows its argument
+template <class T>
+Owned<T> borrowed(T x)
+{
+    x.copy();
+    return Owned{x};
+}
 
 template <class T>
-Owned<T>::~Owned() { x.free(); }
+Owned<T>::Owned(const Owned<T> &x) : obj(x.obj)
+{
+    obj.copy();
+}
+
+//template <class T>
+//Owned<T>::Owned(Owned<T> &&x) : obj(x.obj) {}
 
 template <class T>
-T* Owned<T>::operator->() { return &x; }
+Owned<T>::Owned(T x) : obj(x) {
+    x.copy();
+}
 
 template <class T>
-T Owned<T>::borrow() const { return x; }
+Owned<T> &Owned<T>::operator=(const Owned<T> &rhs)
+{
+    obj.free();
+    obj = rhs.obj;
+    obj.copy();
+    return *this;
+}
+
+template <class T>
+Owned<T>::~Owned()
+{
+    obj.free();
+}
+
+template <class T>
+T* Owned<T>::operator->() {
+    return &obj;
+}
+
+template <class T>
+T const* Owned<T>::operator->() const {
+    return &obj;
+}
+
+template <class T>
+T Owned<T>::borrow() const {
+    return obj;
+}
