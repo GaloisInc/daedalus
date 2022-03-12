dc7ad9e1554089b8951db21a942ad17f59b39351
Mark Tullsen <tullsen@galois.com>
2022-03-11 16:48:20 -0800

    testset: fix how we determine ACCEPT/REJECT from output files.
    - update test_*/results/*.result-actual
    - update test_*/variances.filelist

--- pdf-driver/test/src/run-testset/Tools.hs
+++ pdf-driver/test/src/run-testset/Tools.hs
@@ -32,9 +32,9 @@ validate_T =
   T { t_name          = "validatePDF"
     , t_cmd_exec      = "pdf-hs-driver"
     , t_cmd_mkArgs    = (\f->[f])
-    , t_timeoutInSecs = timeoutInSecs
+    , t_timeoutInSecs = 4*60  -- typically overridden on command-line
     , t_proj          = proj
-    , t_cmp           = matchesRE
+    , t_cmp           = matchesRE -- NB: the result-expected data is a RegExp!
     }
 
   where
@@ -48,25 +48,28 @@ validate_T =
                        RE.defaultExecOpt
                        s
                 
-  timeoutInSecs = 4*60  -- FIXME: be able to specify on command line??
-                        -- currently 8-ish files are timing out (2020-03-eval/*)
   proj :: IO String -> IO String -> IO MetaData-> IO String
-  proj _getOut _getErr getMeta =
+  proj _getStdOut getStdErr getMeta =
     do
+    errs  <- lines <$> getStdErr
     metaD <- getMeta
-    let r = if runtime metaD >= timeoutInSecs*1000 then
-              Timeout
-            else if exitCode metaD == ExitSuccess then
-              Good
-            else
-              Bad
+    let r = case errs of
+              ["ACCEPT",_] -> Good
+              ["REJECT",_] -> Bad
+              []           -> if exitCode metaD == ExitSuccess then
+                                Unclear
+                              else
+                                Timeout
+              _            -> Unclear
+              
     return (show r)
 
 data Result_validate_T =
        Good
      | Bad
-     | Timeout 
-     | NCBUR   -- can be the expected result tho now not 'returned' by
+     | Timeout
+     | Unclear   -- the result is unclear, apparently inconsistent output
+     | NCBUR     -- future (no info at the moment from pdf-hs-driver)
      deriving (Eq, Read, Show)  
 
 
@@ -116,13 +119,12 @@ totext_T =
   T { t_name          = "totext"
     , t_cmd_exec      = "pdf-hs-driver"
     , t_cmd_mkArgs    = (\f->["-t", f])
-    , t_timeoutInSecs = timeoutInSecs
+    , t_timeoutInSecs = 3*60
     , t_proj          = proj
     , t_cmp           = cmp
     }
 
   where
-  timeoutInSecs = 3*60
     
   proj :: IO String -> IO String -> IO MetaData-> IO String
   proj getStdOut _getErr _getMeta =
