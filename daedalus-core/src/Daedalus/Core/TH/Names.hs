c63bd9dda9fd515ca94b811857f1760a448e7260
Iavor Diatchki <iavor.diatchki@gmail.com>
2022-06-22 11:31:19 -0700

    Define union constructr names

--- daedalus-core/src/Daedalus/Core/TH/Names.hs
+++ daedalus-core/src/Daedalus/Core/TH/Names.hs
@@ -11,19 +11,22 @@ import qualified Language.Haskell.TH as TH
 import Daedalus.Panic(panic)
 import Daedalus.Core.Basics
 
-tdeclIdent :: String -> String
-tdeclIdent x =
+typeIdent :: String -> String
+typeIdent x =
   case x of
     a : _ -> if isUpper a then x else 'T' : x
     _     -> panic "tdeclIdent" ["Empty name"]
 
+-- XXX: do we need to do something for anon types?
 dataName :: TName -> TH.Name
-dataName = TH.mkName . tdeclIdent . Text.unpack . tnameText
+dataName = TH.mkName . typeIdent . Text.unpack . tnameText
 
 structConName :: TName -> TH.Name
 structConName = dataName
 
 unionConName :: TName -> Label -> TH.Name
-unionConName = undefined -- XXX
+unionConName n l = TH.mkName (base ++ "_" ++ Text.unpack l)
+  where
+  base = typeIdent (Text.unpack (tnameText n))
 
 
