8529e9482d8ad1165f11ac107bdb39043a8ad70e
Iavor Diatchki <iavor.diatchki@gmail.com>
2022-08-23 12:39:06 +0300

    Factor (well copy) pdf COS parsing into a separate library.
    The main driver does not yet use this because we need to figure out
    how to do compositional parsers (i.e., have one parser use artifacts
    from a separately compiler parser).

---
+++ formats/pdf/new/c++/pdfcos/include/pdfcos/state.hpp
@@ -0,0 +1,96 @@
+#pragma once
+
+#include <map>
+#include <tuple>
+#include <variant>
+#include <unordered_set>
+#include <optional>
+
+#include <pdfcos.h>
+#include <ddl/input.h>
+#include <ddl/owned.h>
+#include <pdfcos/encryption.hpp>
+
+struct Blackhole {};
+
+// Unprocessed declaration inside an object stream
+struct StreamThunk {
+    uint64_t container;
+    uint64_t index;
+    explicit StreamThunk(uint64_t container, uint64_t index);
+
+    bool getDecl(ReferenceTable &refs, uint64_t refid, User::TopDecl *result);
+};
+
+
+// Unprocessed declaration in the file
+struct TopThunk {
+    uint64_t offset;
+
+public:
+    explicit TopThunk(uint64_t offset);
+
+    // Borrows: this, input
+    // Populates owned result
+    // Returns true on success
+    bool getDecl(ReferenceTable &refs, DDL::Input input, User::TopDecl *result);
+};
+
+using oref = std::variant<
+    Blackhole,                // we are currently processing this declaration
+    TopThunk,                 // unprocessed, in the file
+    StreamThunk,              // unprocessed, in an object stream
+    DDL::Owned<User::TopDecl> // processed
+>;
+
+using generation_type = uint16_t;
+
+struct ReferenceEntry {
+    ReferenceEntry(oref value, generation_type gen);
+    oref value;
+    generation_type gen;
+};
+
+
+class ReferenceTable {
+
+private:
+    std::optional<DDL::Owned<DDL::Input>> topinput;
+    std::optional<EncryptionContext> encCtx;
+    std::optional<DDL::Owned<User::Ref>> root;
+
+    void process_xref(std::unordered_set<size_t>*, DDL::Input, DDL::Size, bool top);
+    void process_oldXRef(std::unordered_set<size_t>*, DDL::Input, User::CrossRefAndTrailer, bool top);
+    void process_newXRef(std::unordered_set<size_t>*, DDL::Input, User::XRefObjTable, bool top);
+    void process_trailer(std::unordered_set<size_t>*, DDL::Input, User::TrailerDict);
+    void process_trailer_post(User::TrailerDict);
+    void register_uncompressed_reference(uint64_t refid, generation_type gen, uint64_t offset);
+    void register_compressed_reference(uint64_t refid, uint64_t container, uint64_t index);
+    void register_topdecl(uint64_t refid, generation_type gen, User::TopDecl topDecl);
+    void unregister(uint64_t refid);
+
+public: // temporarily public member
+    std::map<uint64_t, ReferenceEntry> table;
+
+    uint64_t currentObjId;
+    generation_type currentGen;
+
+public:
+    std::optional<EncryptionContext> const& getEncryptionContext() const;
+
+    bool resolve_reference(uint64_t refid, generation_type gen, DDL::Maybe<User::TopDecl> *result);
+
+    std::optional<DDL::Owned<User::Ref>> const& getRoot() const;
+    
+    // owns input
+    void process_pdf(DDL::Input);
+};
+
+struct XrefException : public std::exception {
+    const char * msg;
+    XrefException(const char *msg) : msg(msg) {}
+    const char * what () const throw () override
+    {
+        return msg;
+    }
+};
---
+++ formats/pdf/new/c++/tests/pdfcos-test.cpp
@@ -0,0 +1,45 @@
+#include <ddl/input.h>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <pdfcos.h>
+#include <pdfcos/state.hpp>
+
+int main(int argc, char *argv[]) {
+
+  if (argc < 2) {
+    std::cerr << "USAGE: " << (argc < 1 ? "Need" : argv[0]) << " FILE\n";
+    return 1;
+  }
+
+  std::ifstream fin {argv[1], std::ios::in | std::ios::binary};
+  std::ostringstream sout;
+
+  if (!fin.is_open()) {
+    std::cerr << "Failed to open " << argv[1] << std::endl;
+    return 1;
+  }
+
+  sout << fin.rdbuf();
+  std::string str = std::move(sout.str());
+
+  DDL::Input input{argv[1], str.data(), str.size()};
+
+  ReferenceTable refs;
+
+  try {
+    refs.process_pdf(input);
+
+    for (auto && [refid, val] : refs.table) {
+      User::Ref ref;
+      ref.init(DDL::Integer{refid}, DDL::Integer{val.gen});
+      std::cout << ref << std::endl;
+      ref.free();
+    }
+
+  } catch (XrefException const& e) {
+    std::cerr << "ERROR: [XRef] " << e.what() << std::endl;
+  }
+
+  return 0;
+}
