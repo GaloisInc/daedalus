437d8707d7e055e9c3084341fc90f99570d719f2
Jonathan Daugherty <cygnus@foobox.com>
2022-11-28 10:22:43 -0800

    daedalus-language-server: Main: remove unused imports and pragmas

--- daedalus-language-server/app/Main.hs
+++ daedalus-language-server/app/Main.hs
@@ -1,12 +1,4 @@
-{-# LANGUAGE DeriveGeneric         #-}
-{-# LANGUAGE DeriveAnyClass        #-}
 {-# LANGUAGE LambdaCase            #-}
-{-# LANGUAGE OverloadedStrings     #-}
-{-# LANGUAGE ScopedTypeVariables   #-}
-{-# LANGUAGE RankNTypes            #-}
-{-# LANGUAGE GADTs                 #-}
-{-# LANGUAGE TypeInType            #-}
-{-# LANGUAGE DuplicateRecordFields #-}
 
 -- Based upon lsp/lsp/example/Reactor.hs from the lsp package
 
@@ -26,62 +18,10 @@ and plug it into your client of choice.
 -}
 module Main (main) where
 
-import qualified Data.Text as Text
-import Data.Text (Text)
-
-import           Control.Concurrent.STM.TChan
-import           Control.Concurrent.STM.TVar
-import qualified Control.Exception                     as E
-import           Control.Lens hiding (Iso)
-import           Control.Monad
-import           Control.Monad.IO.Class
-import           Control.Monad.STM
-import           Control.Monad.Reader
-import qualified Data.Aeson                            as J
-import qualified Data.HashMap.Strict                   as H
-import qualified Data.Text                             as T
-import           GHC.Generics (Generic)
-import           Language.LSP.Server
-
-import qualified Language.LSP.Types.Lens       as J
-import           Language.LSP.VFS
-import           System.Exit
-import           System.Log.Logger
-import           Control.Concurrent
-
-import qualified Data.ByteString.Lazy.Char8 as BSL
-
-import Data.Map (Map)
-import qualified Data.Map as Map
-
-
-import Daedalus.Parser.Monad (ParseError(..))
-import Daedalus.Parser (parseFromText)
-
-import Daedalus.Scope (resolveModule, ScopeError(..))
-
-import Daedalus.Type.Monad (TypeError(..), runMTypeM)
-import Daedalus.Module (ModuleException(..))
-import Daedalus.Driver
-import Daedalus.AST (Located(..), ModuleName, Module)
-import Daedalus.Type.AST (TCModule)
-
-import Daedalus.GUID
-import Daedalus.Pass
-import Daedalus.PP
-import Daedalus.SourceRange
-
+import System.Exit
 import CommandLine
-import Data.Maybe (fromMaybe)
-import Daedalus.Type (inferRules)
 
-import Daedalus.LSP.Monad
 import Daedalus.LSP.Server (run)
-
--- ----------------------------------------------------------------------------------------
--- Server monad
-  
--- ---------------------------------------------------------------------
 
 main :: IO ()
 main = do
@@ -89,5 +29,3 @@ main = do
   run >>= \case
     0 -> exitSuccess
     c -> exitWith . ExitFailure $ c
-
-
