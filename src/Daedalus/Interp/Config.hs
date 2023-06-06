d7df123311e6787834c168fded3d703963a0136a
Iavor Diatchki <iavor.diatchki@gmail.com>
2023-06-06 16:29:24 -0700

    Enable multiple errors for now (this should be a flag)

--- src/Daedalus/Interp/Config.hs
+++ src/Daedalus/Interp/Config.hs
@@ -29,7 +29,7 @@ defaultInterpConfig = InterpConfing
 -- | Enables detailed call stacks and traced values.
 detailedErrorsConfig :: InterpConfing
 detailedErrorsConfig = defaultInterpConfig
-  { multiErrors = False
+  { multiErrors = True
   , detailedCallstack = True
   , tracedValues = True
   }
