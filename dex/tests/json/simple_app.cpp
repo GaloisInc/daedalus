ee075d20f9fdf9b24dbf90fc38d03532381127a2
Iavor S. Diatchki <diatchki@galois.com>
2026-02-10 10:16:47 -0800

    Make the example a bit nicer

--- dex/tests/json/simple_app.cpp
+++ dex/tests/json/simple_app.cpp
@@ -1,18 +1,48 @@
-
+#include <iostream>
 #include <vector>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
+#include <unistd.h>
+
 #include "parser/exporter.h"
 
-int main() {
+DDL::Input inputFromFile(const char *file) {
+
+  DDL::Input result{};
+  char *bytes;
+  size_t size;
+
+  int fd = open(file,O_RDONLY);
+  if (fd == -1) return result;
+
+  struct stat info;
+  if (fstat(fd, &info) != 0) goto done;
+
+  size = info.st_size;
+  bytes = (char*)mmap(NULL, size, PROT_READ, MAP_PRIVATE, fd, 0);
+  if (bytes == MAP_FAILED) goto done;
+
+  result = DDL::Input{file,bytes,size};
+
+done:
+  close(fd);
+  return result;
+}
+
+int main(int argc, char* argv[]) {
   DDL::ParseError<DDL::Input> error;
   std::vector<User::JSON_value> results;
-  parseJSON_value_strict(error, results, DDL::Input("(test)", "[1,2,3]"));
+  auto input = argc > 1? inputFromFile(argv[1]) : DDL::Input("(test)", "[1,2,3]");
+  parseJSON_value_strict(error, results, input);
   
   for(auto i : results) {
     auto x = json::exportJSON(i);
-    std::cout << x << "\n";
+    std::cout << std::setw(2) << x << "\n";
   }
   if (results.size() == 0) {
-    std::cout << "parse error\n";
+    std::cerr << "parse error\n";
+    std::cerr << error << "\n";
   }
   
   return 0;
