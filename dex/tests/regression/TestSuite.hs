1514a87a6271942eae09bbedbfdda6014839d121
Iavor S. Diatchki <diatchki@galois.com>
2026-03-03 08:45:59 -0800

    Remove Haskell wrapper, not really needed

--- dex/tests/regression/TestSuite.hs
+++
@@ -1,9 +0,0 @@
-module Main(main) where
-
-import System.Directory(setCurrentDirectory)
-import System.Process(callProcess)
-
-main :: IO ()
-main =
-  do setCurrentDirectory "dex/tests/regression"
-     callProcess "make" ["test"]
