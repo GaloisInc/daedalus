d0b7ef82aeabe3e3cc169e3bb97a9ecea8d7681b
Iavor S. Diatchki <diatchki@galois.com>
2026-05-22 11:54:18 -0700

    Fix comparisons to account for NaN weirdness

--- daedalus-value/src/Daedalus/Value/Compare.hs
+++ daedalus-value/src/Daedalus/Value/Compare.hs
@@ -5,19 +5,33 @@ import Daedalus.Value.Type
 import Daedalus.Value.Utils
 
 vEq :: Value -> Value -> Value
-vEq = tracedFun \x y -> VBool (vCompare x y == EQ)
+vEq = tracedFun \x y ->
+  VBool $ if hasNaN x y then False else vCompare x y == EQ
 
 vNeq :: Value -> Value -> Value
-vNeq = tracedFun \x y -> VBool (vCompare x y /= EQ)
+vNeq = tracedFun \x y ->
+  VBool $ if hasNaN x y then True else vCompare x y /= EQ
 
 vLt :: Value -> Value -> Value
-vLt = tracedFun \x y -> VBool (vCompare x y == LT)
+vLt = tracedFun \x y ->
+  VBool $ if hasNaN x y then False else vCompare x y == LT
 
 vGt :: Value -> Value -> Value
-vGt = tracedFun \x y -> VBool (vCompare x y == GT)
+vGt = tracedFun \x y ->
+  VBool $ if hasNaN x y then False else vCompare x y == GT
 
 vLeq :: Value -> Value -> Value
-vLeq = tracedFun \x y -> VBool (vCompare x y /= GT)
+vLeq = tracedFun \x y ->
+  VBool $ if hasNaN x y then False else vCompare x y /= GT
 
 vGeq :: Value -> Value -> Value
-vGeq = tracedFun \x y -> VBool (vCompare x y /= LT)
+vGeq = tracedFun \x y ->
+  VBool $ if hasNaN x y then False else vCompare x y /= LT
+
+hasNaN :: Value -> Value -> Bool
+hasNaN a b = isNaN' a || isNaN' b
+  where
+  isNaN' (VFloat x)    = isNaN x
+  isNaN' (VDouble x)   = isNaN x
+  isNaN' (VTraced v _) = isNaN' v
+  isNaN' _             = False
